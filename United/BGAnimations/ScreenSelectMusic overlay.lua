edc67a6315ded37db969ea66c117423990587509
Jolomann <33073129+Jolomann@users.noreply.github.com>
2020-08-15 01:27:11 +0200

    Song select placeholders pt3
    Add profile panel with many different banners available. Placeholder avatar. Banner is set to randomly pick - will be changed. Pack name also dynamically updates woo

--- United/BGAnimations/ScreenSelectMusic overlay.lua
+++ United/BGAnimations/ScreenSelectMusic overlay.lua
@@ -1,5 +1,20 @@
 local t = Def.ActorFrame {}
 
+--Avatar/Profile definitions
+local theTexture = nil
+
+if theTexture == nil then
+  avatarTexture = THEME:GetPathG("","Profile/Avatars/default")
+else
+  avatarTexture = asdfasdfasdf
+end
+
+if theTexture == nil then
+  bannerTexture = THEME:GetPathG("","Profile/Banners/" .. tostring(math.random(1,40)) .. ".png")
+else
+  avatarTexture = asdfasdfasdf
+end
+
 --Banner background
 t[#t + 1] =
   Def.Quad{
@@ -331,12 +346,21 @@ t[#t + 1] =
   LoadFont("_raleway black 48px") ..
     {
       InitCommand=function(self)
-        self:diffusealpha(0.51):halign(1):valign(1):xy(1840,140)
+        self:diffusealpha(0.51):halign(1):valign(1):xy(1840 + 5,140)
       end,
 
       OnCommand=function(self)
         local song = GAMESTATE:GetCurrentSong()
         self:settext(song:GetGroupName())
+      end,
+
+      CurrentSongChangedMessageCommand=function(self)
+        local song = GAMESTATE:GetCurrentSong()
+        if song == nil then
+          return
+        else
+          self:settext(song:GetGroupName())
+        end
       end
   }
 
@@ -454,5 +478,142 @@ t[#t + 1] =
       self:halign(0):valign(0):xy(640,155):zoomto(150,35):diffuse(color("#e3b22a")):diffusealpha(1)
     end
   }
+
+--Profile avatar
+t[#t + 1] =
+  Def.Sprite{
+  	Texture=avatarTexture;
+    InitCommand=function(self)
+      self:halign(0):valign(0):diffusealpha(1):zoomto(64,64):xy(1316,24)
+    end
+  }
+
+--Profile banner
+t[#t + 1] =
+  Def.Sprite{
+    Texture=bannerTexture;
+    InitCommand=function(self)
+      self:halign(0):valign(0):diffusealpha(1):zoomto(250,60):xy(1380,26)
+    end
+  }
+
+--Profile frame
+t[#t + 1] =
+  Def.Quad{
+    Name="ProfileFrameTop";
+    InitCommand=function(self)
+      self:halign(0):valign(0):xy(1316,24):zoomto(314,2):diffuse(color("#3c3c3c")):diffusealpha(1)
+    end
+  }
+
+t[#t + 1] =
+  Def.Quad{
+    Name="ProfileFrameRight";
+    InitCommand=function(self)
+      self:halign(1):valign(0):xy(1630,26):zoomto(2,60):diffuse(color("#3c3c3c")):diffusealpha(1)
+    end
+  }
+
+t[#t + 1] =
+  Def.Quad{
+    Name="ProfileFrameBottom";
+    InitCommand=function(self)
+      self:halign(0):valign(0):xy(1316,86):zoomto(314,2):diffuse(color("#3c3c3c")):diffusealpha(1)
+    end
+  }
+
+t[#t + 1] =
+  Def.Quad{
+    Name="ProfileFrameLeft";
+    InitCommand=function(self)
+      self:halign(0):valign(0):xy(1316,26):zoomto(2,60):diffuse(color("#3c3c3c")):diffusealpha(1)
+    end
+  }
+
+t[#t + 1] =
+  Def.Quad{
+    Name="ProfileFrameDivider";
+    InitCommand=function(self)
+      self:halign(1):valign(0):xy(1380,26):zoomto(2,60):diffuse(color("#3c3c3c")):diffusealpha(1)
+    end
+  }
+
+t[#t + 1] =
+  LoadFont("Common Normal") ..
+    {
+      Name = "Name",
+      InitCommand = function(self)
+        self:xy(643, 14):maxwidth(400):halign(0):zoom(0.35):valign(0)
+      end,
+      SetCommand = function(self)
+        self:settextf("%s", profileName)
+        if profileName == "Default Profile" or profileName == "" then
+          easyInputStringWithFunction(
+            translated_info["ProfileNew"],
+            64,
+            false,
+            setnewdisplayname
+          )
+        end
+      end,
+      MouseLeftClickMessageCommand = function(self)
+        if isOver(self) and not SCREENMAN:get_input_redirected(PLAYER_1) then
+          easyInputStringWithFunction(translated_info["NameChange"], 64, false, setnewdisplayname)
+        end
+      end,
+      ProfileRenamedMessageCommand = function(self, params)
+        self:settextf("%s", profileName)
+      end
+    }
+
+  --MSD string
+t[#t + 1] =
+    LoadFont("Common Normal") ..
+    {
+      Name = "Rating",
+      InitCommand = function(self)
+        self:xy(643, 24):maxwidth(400):halign(0):zoom(0.18):valign(0)
+      end,
+      SetCommand = function(self)
+        self:settext("LV.")
+      end,
+      MouseLeftClickMessageCommand = function(self)
+        if isOver(self) and not SCREENMAN:get_input_redirected(PLAYER_1) then
+          easyInputStringWithFunction(translated_info["NameChange"], 64, false, setnewdisplayname)
+        end
+      end,
+    }
+
+
+  --Player rating string
+t[#t + 1] =
+    LoadFont("Common Normal") ..
+    {
+      Name = "Rating",
+      InitCommand = function(self)
+        self:xy(636, 24):maxwidth(400):halign(0):zoom(0.6):valign(0)
+      end,
+      SetCommand = function(self)
+        self:settextf("%5.0f", playerRating)
+      end,
+      MouseLeftClickMessageCommand = function(self)
+        if isOver(self) and not SCREENMAN:get_input_redirected(PLAYER_1) then
+          easyInputStringWithFunction(translated_info["NameChange"], 64, false, setnewdisplayname)
+        end
+      end,
+    }
+
+
+t[#t + 1] =
+    LoadFont("Common Bold") ..
+    {
+      Name = "Rating2",
+      InitCommand = function(self)
+        self:valign(1):halign(1):xy(642+120-2, 24+10):zoom(0.3)
+      end,
+      SetCommand = function(self)
+        self:settext(math.floor((math.fmod(playerRating, 1)+0.005)*100) .. "%")
+      end,
+    }
 
 return t
